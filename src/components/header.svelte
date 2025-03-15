1e83ab8d5d37552f5540b8367d3a9f2407003bd1
KaniyaSimeji <dev@kanium.me>
2025-03-15 15:55:54 +0900

    Rename project and fix base path handling
    Renamed the project in `package.json` and adjusted Svelte's base path configuration to correctly handle development and production environments. Added a format check step to the GitHub Actions workflow. Updated header links to use the dynamic base path for better compatibility.

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+import { base } from "$app/paths";
+</script>
+
 <div class="border-b-4 border-aqn">
     <ul class="flex flex-row m-3">
-        <li class="basis-32"><a href="/">トップページ</a></li>
-        <li class="basis-16"><a href="/about">概要</a></li>
+        <li class="basis-32"><a href="{base}/">トップページ</a></li>
+        <li class="basis-16"><a href="{base}/about">概要</a></li>
     </ul>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,7 @@ const config = {
 			fallback: "404.html",
 		}),
 		paths: {
-			base:
-				process.env.NODE_ENV === "development" ? "/" : process.env.BASE_PATH,
+			base: process.env.NODE_ENV === "development" ? "" : process.env.BASE_PATH,
 		},
 	},
 };

03b3fc706aac2fa947c2d51584274043614dbd7a
KaniyaSimeji <dev@kanium.me>
2025-03-15 16:31:38 +0900

    Add additional details and links to the About page
    Expanded the About page with new sections, including participation instructions, site maintenance information, and licensing details. Added hyperlinks for external resources and emphasized collaboration opportunities through Discord and GitHub. Enhanced the user experience with improved formatting and underlined links.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,22 @@
 <p>ブログやプレゼンテーションなどの発表活動を通して、東方Projectの魅力を見つける企画です。</p>
 <h2 class="text-2xl my-2.5">詳しく</h2>
 <p class="break-all">
-    <a href="https://touhou-project.news/touhou_project/">東方Project</a>に関するブログ記事やプレゼンテーション、動画などを作成し、それを発表するという企画です。<br />
-    また、さながらアドベントカレンダーのような形式で、毎週日曜日に発表していくことで東方Projectの姿を見出していく狙いもあります。
+    <a href="https://touhou-project.news/touhou_project/" class="underline decoration-2 decoration-aqn">東方Project</a>に関するブログ記事やプレゼンテーション、動画などを作成し、それを発表するという企画です。<br />
+    また、さながらアドベントカレンダーのような形式で、毎週日曜日に発表活動を行うことで東方Projectの姿を見出していく狙いもあります。
 </p>
-
+<p>ざっくりとした説明は<a href="https://note.com/kanium3/n/n35bbe478eeab" class="underline decoration-2 decoration-aqn">この記事</a>にも載っているので見てみてください。</p>
+<h2 class="text-2xl my-2.5">参加したい場合</h2>
+<p class="break-all">
+    この企画は、東方好きの集うDiscordサーバー<a href="https://discord.gg/gdbqUJ84Wp" class="underline decoration-2 decoration-aqn">「東方談笑所」</a>が運営しております。
+    参加したい場合はリンクよりサーバーへの参加お願いします。
+</p>
+<h2 class="text-2xl my-2.5">このサイトについて</h2>
+<p class="break-all">
+    このサイトの保守要員は常に人手不足です。PullRequestも受け付けているので、協力して頂ける方はぜひよろしくお願いします。
+    </p>
+<p class="break-all">
+    また、運営の方で手伝ってくれる方も常に募集しています。そちらはDiscordのほうからご一報お願いします。
+</p>
+<p class="break-all">日々のメンテナンスや機能追加などのリクエストは<a href="https://github.com/kanium3/embody_touhou" class="underline decoration-2 decoration-aqn">GitHub</a>からIssueのほどよろしくお願いします。</p>
+<h3 class="text-xl my-2.5">ライセンス等</h3>
+<p class="break-all">当ウェブサイトはMITライセンスの下で提供されています。ただし各発表物の内容やタイトルについては製作者に帰属します。</p>

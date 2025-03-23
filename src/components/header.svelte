3defab87797dc437c7ce275b234d3c37cd53e512
kanium3 <dev@kanium.me>
2025-03-23 21:06:59 +0900

    feat: In hoving, apply underline decoration

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -4,7 +4,7 @@ import { base } from "$app/paths";
 
 <div class="border-b-4 border-aqn">
     <ul class="flex flex-row m-3">
-        <li class="basis-32"><a href="{base}/">トップページ</a></li>
-        <li class="basis-16"><a href="{base}/about">概要</a></li>
+        <li class="basis-32 hover:underline decoration-1 decoration-reimu-in-koumakyo underline-offset-2"><a href="{base}/">トップページ</a></li>
+        <li class="basis-16 hover:underline decoration-1 decoration-reimu-in-koumakyo underline-offset-2"><a href="{base}/about">概要</a></li>
     </ul>
 </div>
